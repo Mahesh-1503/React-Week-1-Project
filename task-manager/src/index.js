17140c81dc33c8051d7cc098fcf9c621f86db53f
Mahesh-1503 <mnopxyz090@gmail.com>
2024-10-13 12:30:52 +0530

    -M

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
 import App from './App';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.render(
+// Create a root for React
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create the root using createRoot
+
+// Render the application
+root.render(
   <React.StrictMode>
     <App />
     <ToastContainer />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
