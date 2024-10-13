fc45fa21cee7d422ad979f2e4e868cb7332a23a2
Mahesh-1503 <mnopxyz090@gmail.com>
2024-10-13 12:58:48 +0530

    -m

--- task-manager/src/components/TaskList.js
+++ task-manager/src/components/TaskList.js
@@ -1,11 +1,12 @@
 // src/components/TaskList.js
 import React from 'react';
+import PropTypes from 'prop-types'; // Import PropTypes for type checking
 import Task from './Task';
 
 const TaskList = ({ tasks, onUpdateStatus, onDeleteTask }) => {
   return (
     <div className="task-list">
-      {tasks.length ? (
+      {tasks.length > 0 ? (
         tasks.map((task) => (
           <Task 
             key={task.id} 
@@ -19,6 +20,22 @@ const TaskList = ({ tasks, onUpdateStatus, onDeleteTask }) => {
       )}
     </div>
   );
+};
+
+// PropTypes for type checking
+TaskList.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string,
+      status: PropTypes.string.isRequired,
+      addedDate: PropTypes.string.isRequired,
+      completedDate: PropTypes.string,
+    })
+  ).isRequired,
+  onUpdateStatus: PropTypes.func.isRequired,
+  onDeleteTask: PropTypes.func.isRequired,
 };
 
 export default TaskList;
